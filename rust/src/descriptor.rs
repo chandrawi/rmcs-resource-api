f77ba32e90062788420ba96558d3851daa50a267
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2023-05-26 17:26:41 +0700

    Create descriptor for resource protobufs

---
+++ rust/src/descriptor.rs
@@ -0,0 +1,34 @@
+pub mod model {
+    pub const DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("model_descriptor");
+}
+
+pub mod device {
+    pub const DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("device_descriptor");
+}
+
+pub mod group {
+    pub const DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("group_descriptor");
+}
+
+pub mod data {
+    pub const DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("data_descriptor");
+}
+
+pub mod buffer {
+    pub const DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("buffer_descriptor");
+}
+
+pub mod slice {
+    pub const DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("slice_descriptor");
+}
+
+pub mod log {
+    pub const DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("log_descriptor");
+}
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod descriptor;
 pub mod common;
 pub mod model;
 pub mod device;
