f61cc6f5c2a57577fce478e81bfc041d02707744
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2025-07-03 14:14:01 +0700

    Update dependency

--- python/src/rmcs_resource_api/device_pb2_grpc.py
+++ python/src/rmcs_resource_api/device_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from rmcs_resource_api import device_pb2 as rmcs__resource__api_dot_device__pb2
 
-GRPC_GENERATED_VERSION = '1.71.0'
+GRPC_GENERATED_VERSION = '1.73.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
